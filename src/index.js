aa3d368fc1c734abe7e36ff67ea9e1944af45dc7
Rinat Arifullin <74796640+Rinat-Arifullin@users.noreply.github.com>
2020-12-26 22:16:24 +0300

    Finish base framework functional (#3)
    Co-authored-by: Rinat <arifullin.rinat2652@mail.ru>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import './module.js';
+import {Excel} from './components/excel/Excel.js';
+import {Header} from './components/header/header.js';
+import {Toolbar} from './components/Toolbar/Toolbar.js';
+import {Formula} from './components/Formula/Formula.js';
+import {Table} from './components/Table/Table.js';
 import './scss/index.scss';
-console.log('Working');
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+});
+excel.render();
